f29df1a1377f008b313d4ae896d7d73f907f737a
Dawid Mariankowski <dawid24.9@o2.pl>
2019-04-06 21:51:18 +0200

    Create pxToRemConvert.js

---
+++ pxToRemConvert.js
@@ -0,0 +1,14 @@
+function remCaclculate(px) {
+	let browserValue = 16;
+
+	function countEmValue() {
+		return px / browserValue;
+	}
+
+	return countEmValue;
+}
+
+let smallText = 13;
+let mediumText = 16;
+let largeText = 19;
+let hugeText = 38;
